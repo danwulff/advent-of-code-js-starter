fb01284e4500e9cc25fff38af19a24d4e75b2d58
Mirasii <meatheat_100mean@hotmail.co.uk>
2022-12-07 17:55:23 +0000

    day1 solution

--- src/day1/day1.js
+++ src/day1/day1.js
@@ -1,7 +1,23 @@
 function day1(input) {
+  dataArray = input.replaceAll('\r','').split('\n');
+  var sortedArray = [];
+    var currCount = 0;
+    var elf = 1;
+    for (line of dataArray) {
+        if (line == '') {
+            sortedArray.push({"elf":elf,"count": currCount});
+            currCount = 0;
+            elf++;
+        } else {
+            currCount += parseInt(line);
+        }
+    }
+    sortedArray.push({"elf":elf,"count": currCount});
+    sortedArray.sort(function(a,b){return b.count-a.count});
+    const sum = sortedArray.slice(0,3).reduce((partialSum, a) => partialSum + a.count, 0);
   return {
-    partOne: null,
-    partTwo: null,
+    partOne: `elf ${sortedArray[0].elf} had the most with a total of ${sortedArray[0].count}`,
+    partTwo: `elves ${sortedArray[0].elf}, ${sortedArray[1].elf} and ${sortedArray[2].elf} were the top 3 with a combined total of ${sum}`,
   };
 }
 
--- src/day1/day1.test.js
+++ src/day1/day1.test.js
@@ -1,13 +1,22 @@
 const { day1 } = require("./day1");
 
-let input = `replace me
-some input string to mimic
-the input files
+let input = `1000
+2000
+3000
 
-`;
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
 
 test("day1", () => {
   const result = day1(input);
-  expect(result.partOne).toBe(null);
-  expect(result.partTwo).toBe(null);
+  expect(result.partOne).toBe(24000);
+  expect(result.partTwo).toBe(45000);
 });
